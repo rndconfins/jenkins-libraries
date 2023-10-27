666243f978a45374b4ca462a62656cf953f30b5b
rndconfins <142009650+rndconfins@users.noreply.github.com>
2023-10-27 16:38:29 +0700

    Update GenerateDeploymentYamlStepV2.groovy

--- vars/GenerateDeploymentYamlStepV2.groovy
+++ vars/GenerateDeploymentYamlStepV2.groovy
@@ -485,9 +485,9 @@ def call(Map config = [:]) {
             sh "rm ./configmap.yaml"
             writeYaml(data: configmap, file: "configmap.yaml")
 
-            //sh "type ./deployment.yaml"
-            //sh "type ./service.yaml"
-            //sh "type ./configmap.yaml"
+            sh "type ./deployment.yaml"
+            sh "type ./service.yaml"
+            sh "type ./configmap.yaml"
 
             // Simpan objek gabungan ke dalam file baru
             writeYaml(datas: [deployment, service, configmap], file: "deploymentservice.yaml")
