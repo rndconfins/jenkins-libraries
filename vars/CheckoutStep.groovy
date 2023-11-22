d40234987ab349860ce9b670a1f2d1449cb7f22c
rndconfins <142009650+rndconfins@users.noreply.github.com>
2023-11-22 21:01:02 +0700

    Update CheckoutStep.groovy

--- vars/CheckoutStep.groovy
+++ vars/CheckoutStep.groovy
@@ -11,25 +11,7 @@
 */
 def call(Map config = [:]) {
     if (config.cleanWorkspace) {
-        //cleanWs() 
-        if (!isUnix())
-        {
-            // Menentukan pola file yang ingin dikecualikan
-            def excludedFiles = ['GeneralConfig.json', 'GeneralConfig_FrontEnd.json']
-            
-            // Membersihkan workspace kecuali file tertentu
-            bat 'rmdir /s /q *'
-            bat "for %%I in (${excludedFiles.join(' ')}) do del /q %%I"
-        }
-        else
-        {
-            // Menentukan pola file yang ingin dikecualikan
-            def excludedFiles = ['GeneralConfig.json', 'GeneralConfig_FrontEnd.json']
-    
-            // Membersihkan workspace kecuali file tertentu
-                sh "find . -type f \\( ! -name ${excludedFiles.collect { "-name '${it}'" }.join(' -a ') } \\) -exec rm -f {} \\;"
-                sh "find . -type d -empty -delete"
-        }
+        cleanWs()
     }
     
     withCredentials([usernamePassword(credentialsId: config.credentialsId, passwordVariable: 'PASS', usernameVariable: 'USER')]) {
