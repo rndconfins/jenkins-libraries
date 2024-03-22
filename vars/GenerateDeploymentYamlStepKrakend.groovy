e335de0e2e3729c9747773fe3a2a41a3f9b6fd6a
kusnendi <kusnendi.muhamad@ad-ins.com>
2024-03-22 22:35:45 +0700

    Update generate deployment krakend

--- vars/GenerateDeploymentYamlStepKrakend.groovy
+++ vars/GenerateDeploymentYamlStepKrakend.groovy
@@ -29,7 +29,8 @@ import groovy.json.JsonBuilder
 
 def call(Map config = [:]) {
     if (isUnix()) {
-        configFileProvider([configFile(fileId: 'kube-krakend-deployment', targetLocation: './deployment.yaml', variable: 'deployment'), configFile(fileId: 'kube-krakend-service', targetLocation: './service.yaml', variable: 'service'), configFile(fileId: 'kube-configmap-yaml', targetLocation: './configmap.yaml', variable: 'configmap')]) 
+        configFileProvider([configFile(fileId: 'kube-krakend-deployment', targetLocation: './deployment.yaml', variable: 'deployment'), configFile(fileId: 'kube-krakend-service', targetLocation: './service.yaml', variable: 'service'), configFile(fileId: 'kube-configmap-yaml', targetLocation: './configmap.yaml', variable: 'configmap'),
+        configFile(fileId: 'kube-krakend-env', targetLocation: './configenv.yaml', variable: 'configenv')]) 
         {
             // Namespace
             Map namespace = [apiVersion: "v1", kind: "Namespace", metadata: [name: config.namespace]]
@@ -48,11 +49,14 @@ def call(Map config = [:]) {
             deployment.spec.template.metadata.labels.app = config.deploymentName
             deployment.spec.template.spec.volumes[0].name = """${config.deploymentName}-volume"""
             deployment.spec.template.spec.volumes[0].configMap.name = """${config.deploymentName}-appsettings"""
+            deployment.spec.template.spec.volumes[1].name = """${config.deploymentName}-env-volume"""
+            deployment.spec.template.spec.volumes[1].configMap.name = """${config.deploymentName}-env"""
             deployment.spec.template.spec.containers[0].name = config.deploymentName
             deployment.spec.template.spec.containers[0].image = config.imageName
             deployment.spec.template.spec.containers[0].volumeMounts[0].name = """${config.deploymentName}-volume"""
             deployment.spec.template.spec.containers[0].volumeMounts[0].mountPath = "/etc/krakend/${config.configMapFileName}"
             deployment.spec.template.spec.containers[0].volumeMounts[0].subPath = config.configMapFileName
+            deployment.spec.template.spec.containers[0].volumeMounts[1].name = """${config.deploymentName}-env-volume"""
 
             sh "rm ./deployment.yaml"
             writeYaml(data: deployment, file: "deployment.yaml")
@@ -82,12 +86,21 @@ def call(Map config = [:]) {
             sh "rm ./configmap.yaml"
             writeYaml(data: configmap, file: "configmap.yaml")
 
+            // ConfigMap .env
+            def configenv = readYaml(file: 'configenv.yaml')
+            configenv.metadata.name = """${config.deploymentName}-env"""
+            configenv.metadata.namespace = config.namespace
+
+            sh "rm ./configenv.yaml"
+            writeYaml(data: configenv, file: "configenv.yaml")
+
             sh "cat ./deployment.yaml"
             sh "cat ./service.yaml"
             sh "cat ./configmap.yaml"
+            sh "cat ./configenv.yaml"
 
             // Simpan objek gabungan ke dalam file baru
-            writeYaml(datas: [deployment, service, configmap], file: "deploymentservice.yaml")
+            writeYaml(datas: [deployment, service, configmap, configenv], file: "deploymentservice.yaml")
                             
             sh "cat ./deploymentservice.yaml"
         }
