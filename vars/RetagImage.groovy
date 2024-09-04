cd09081c07a9ff455f4ac87945e385581228dbf1
rndconfins <142009650+rndconfins@users.noreply.github.com>
2024-09-04 12:03:37 +0700

    Update RetagImage.groovy

--- vars/RetagImage.groovy
+++ vars/RetagImage.groovy
@@ -10,7 +10,7 @@ def call(Map config = [:]) {
 
     def arr = "${config.tagImage}".split(";")
     config.oldImage = config.oldImage.replaceAll("/null/", "/").replaceAll("//", "/").replaceFirst(":/+", "://")
-    config.newImage = config.imageName.replaceAll("/null/", "/").replaceAll("//", "/").replaceFirst(":/+", "://")
+    config.newImage = config.newImage.replaceAll("/null/", "/").replaceAll("//", "/").replaceFirst(":/+", "://")
     arr.each{ element ->
         sh "docker tag ${config.oldImage} ${config.newImage}:${element}"
     }
