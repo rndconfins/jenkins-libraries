464ce1b8304d209a9e5fcf8ab594954739c77d25
raxell.ri <raxell.ri@ad-ins.com>
2026-04-10 11:10:43 +0700

    PushToSharepointStepV2: git add --relative

--- vars/PushToSharepointStepV2.groovy
+++ vars/PushToSharepointStepV2.groovy
@@ -10,7 +10,7 @@ def call(Map config = [:]) {
     
     dir("./$BUILD_PATH") {
         bat "git add --force ."
-        bat "git diff --cached --name-only > list.tmp"
+        bat "git diff --cached --name-only --relative > list.tmp"
         bat "%SCRIPTS%\\psrun.bat get-diff.ps1 $BUILD_NUMBER \"$WORKSPACE\\$BUILD_PATH\" "
         bat "%SCRIPTS%\\psrun.bat uploadV2.ps1 \"Released Package/CONFINS/$JOB_NAME\" $BUILD_NUMBER \"$WORKSPACE\\$BUILD_PATH\" \"$WORKSPACE\\$BUILD_PATH\\list.tmp\" ${destination}"
         bat "del \"$WORKSPACE\\$BUILD_PATH\\list.tmp\""
